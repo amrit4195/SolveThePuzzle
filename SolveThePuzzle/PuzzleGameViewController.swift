ccc6382fea6042cd04c1a43571448616c5ee4ae4
Michael Hartanto <hartantomichael25@yahoo.com>
2017-04-07 01:40:20 +1000

    ver 0.1
    - add custom transition to different page
    
    - draft design
    
    - prepare blink animation

---
+++ SolveThePuzzle/PuzzleGameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PuzzleGameViewController.swift
+//  SolveThePuzzle
+//
+//  Created by Michael Hartanto on 4/6/17.
+//  Copyright © 2017 Amritpal Singh. All rights reserved.
+//
+
+import UIKit
+
+class PuzzleGameViewController: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SolveThePuzzle/SolveThePuzzle/ViewController.swift
+++ SolveThePuzzle/SolveThePuzzle/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var demoPuzzleImageView: UIImageView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var userTimeLabel: UILabel!
+    @IBOutlet weak var clickToStartButton: UIButton!
+    @IBOutlet weak var logoImageView: UIImageView!
+    /*
+    @IBAction func click(_ sender: AnyObject) {
+        
+        print("A")
+    }*/
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // set image for the demo puzzle
+        demoPuzzleImageView.contentMode = .scaleAspectFit
+        demoPuzzleImageView.image = #imageLiteral(resourceName: "app_image")
+     
+        /*
+        // blinking animation
+        UIView.animate(withDuration: 1, delay: 0.0, options: [.repeat,.autoreverse,.allowUserInteraction], animations: {
+            self.clickToStartButton.alpha = 0.1 //disappear
+            self.clickToStartButton.alpha = 1.0 // appear
+            }, completion: nil)
+        
+         */
+    
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        /*
+        // blinking animation
+        UIView.animate(withDuration: 1, delay: 0.0, options: [.repeat,.autoreverse,.allowUserInteraction], animations: {
+            self.clickToStartButton.alpha = 0.1 //disappear
+            self.clickToStartButton.alpha = 1.0 // appear
+            }, completion: nil)
+        */
+    }
+    
+    
+    // changing view controller
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let newViewController: PuzzleGameViewController = segue.destination as! PuzzleGameViewController
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func exitToHomeScene(sender: UIStoryboardSegue){
+        
+    }
+    
+    // override the unwind function
+    override func unwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        let segue = UnwindScaleSegue(identifier: unwindSegue.identifier, source: unwindSegue.source, destination: unwindSegue.destination)
+        segue.perform()
+    }
+    
 
 }
 
