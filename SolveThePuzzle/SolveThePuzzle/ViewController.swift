9e27c932ae0e456c435f1fd2aedd1296ea8c978c
Michael Hartanto <hartantomichael25@yahoo.com>
2017-04-16 01:00:19 +1000

    Help Page
    Content: Blank
    Transition: Done

--- SolveThePuzzle/SolveThePuzzle/ViewController.swift
+++ SolveThePuzzle/SolveThePuzzle/ViewController.swift
@@ -11,21 +11,22 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var demoPuzzleImageView: UIImageView!
-    @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var userTimeLabel: UILabel!
-    @IBOutlet weak var clickToStartButton: UIButton!
+    // Outlets for the main screen
     @IBOutlet weak var logoImageView: UIImageView!
-    var soundPlayer: AVAudioPlayer?
-    
-    /*
-    @IBAction func click(_ sender: AnyObject) {
-        
-        print("A")
-    }*/
-    
+    @IBOutlet weak var userTimeLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var clickToStartButton: UIButton!
 
+    // Outlets for the help popup view
+    @IBOutlet weak var closeHelpViewButton: UIButton!
+    @IBOutlet weak var backgroundButton: UIButton!
+    @IBOutlet weak var centerXPopupConstraint: NSLayoutConstraint!
+    @IBOutlet weak var helpPopupView: UIView!
     
+    // @IBOutlet weak var demoPuzzleImageView: UIImageView!
+    
+    // Initializing Variables
+    var soundPlayer: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +42,11 @@ class ViewController: UIViewController {
         }
             // Catch an error if the playback has an issue
         catch{print("Player does not work for some reason")}
+        
+        // Set up the help popup view design
+        helpPopupView.layer.cornerRadius = 20
+        helpPopupView.layer.masksToBounds = true
+    
         
         // set image for the demo puzzle
         //demoPuzzleImageView.contentMode = .scaleAspectFit
@@ -69,21 +75,49 @@ class ViewController: UIViewController {
     }
     
     
-    // changing view controller
+    // Action to show the help popup view
+    // - Show view by setting the X contraint to be 0
+    // - Start Sliding Animation
+    @IBAction func showHelpPopup(_ sender: AnyObject) {
+        
+        centerXPopupConstraint.constant = 0
+        
+        // Slide In Animation
+        // - Layout the subview immediately
+        // - Set background button alpha to 0.5 
+        //   (this button is also used to close the view and set 0.5 alpha to
+        //   let all the things behind it to have a whitish color)
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+            self.backgroundButton.alpha = 0.5
+        })
+    }
+    
+    // Action to dismiss the help popup view
+    @IBAction func closeHelpPopup(_ sender: AnyObject) {
+        centerXPopupConstraint.constant =  -500
+        
+        // Slide Out Animation
+        // - Layout the subview immediately
+        // - Set background button alpha back to 0 (Unviewable and untouchable)
+        UIView.animate(withDuration: 0.1, animations: {
+            self.view.layoutIfNeeded()
+            self.backgroundButton.alpha = 0
+        })
+
+    }
+    
+    // Open the puzzle Game View Controller
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let newViewController: PuzzleGameViewController = segue.destination as! PuzzleGameViewController
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    // Go back to Home Page
     @IBAction func exitToHomeScene(sender: UIStoryboardSegue){
         
     }
     
-    // override the unwind function
+    // Override the unwind function
     override func unwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
         let segue = UnwindScaleSegue(identifier: unwindSegue.identifier, source: unwindSegue.source, destination: unwindSegue.destination)
         segue.perform()
